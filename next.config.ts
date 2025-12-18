774fc36ccd2e40c1248bc1b599193812b14a987c
TousifIqbal685 <tousifiqbal10083@gmail.com>
2025-12-19 04:07:18 +0600

    nth

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // 1. Keep Standalone mode
-  output: 'standalone',
-
-  // 2. Disable Image Optimization (Save CPU)
-  images: {
-    unoptimized: true,
-  },
-  
-  // 3. Disable Linting during build (Save RAM)
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
-  // 4. Disable TypeScript errors during build (Save RAM)
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-
-  // 5. Disable Source Maps (Save Disk & RAM)
-  productionBrowserSourceMaps: false,
+  /* config options here */
 };
 
 export default nextConfig;
